c769ef0bab193abc28b7e62dfa8ec00fc1ea0363
Zachary Pitcher <zackpi@mit.edu>
2020-01-16 11:48:40 -0500

    async face randomization

--- _site/assets/js/about.js
+++ _site/assets/js/about.js
@@ -1,60 +1,94 @@
 
-// set up canvas
-;(function () {
-	let canvas, ctx;
+function loadImage(src) {
+	return new Promise((resolve, reject) => {
+		const image_to_load = new Image();
+		image_to_load.onload = () => { resolve(image_to_load); };
+		image_to_load.onerror = err => { reject(err); }
+		image_to_load.src = src;
+	});
+}
 
-	function init () {
-		hidden_canvas = document.getElementById('hidden-canvas');
-		hidden_ctx = hidden_canvas.getContext('2d');
-		canvas = document.getElementById('portrait-canvas');
-		ctx = canvas.getContext('2d');
 
-		var img_silhouette = new Image();
-		var img_shirt = new Image();
-		var img_collar = new Image();
-		var img_neck = new Image();
-		var img_head = new Image();
-		var img_face = new Image();
-		var img_eyes = new Image();
+let canvas, ctx;
+let hidden_canvas, hidden_ctx;
 
-		function randLayer(layer) {
-			hidden_ctx.drawImage(layer, 0, 0);
-			img = hidden_ctx.getImageData(0, 0, hidden_canvas.width, hidden_canvas.height);
-			virtual = ctx.getImageData(0, 0, canvas.width, canvas.height);
-			hidden_ctx.clearRect(0, 0, hidden_canvas.width, hidden_canvas.height);
-			
-			var i;
-			var r = 255 * (Math.random() > 0.5);
-			var g = 255 * (Math.random() > 0.5);
-			var b = 255 * (Math.random() > 0.5);
-			var a = 0.3;
-			for (i = 0; i < img.data.length; i += 4) {
-				img.data[i] = r * a * img.data[i] + virtual.data[i];
-				img.data[i+1] = g * a * img.data[i+1] + virtual.data[i+1];
-				img.data[i+2] = b * a * img.data[i+2] + virtual.data[i+2];
-				img.data[i+3] = 255 * a;
-			}
+let img_silhouette;
+let img_shirt;
+let img_collar;
+let img_neck;
+let img_head;
+let img_face;
+let img_eyes;
 
-			ctx.putImageData(img, 0, 0);
-		}
 
-		img_silhouette.onload = function() { randLayer(img_silhouette); };
-		img_shirt.onload = function() { randLayer(img_shirt); };
-		img_collar.onload = function() { randLayer(img_collar); };
-		img_neck.onload = function() { randLayer(img_neck); };
-		img_head.onload = function() { randLayer(img_head); };
-		img_face.onload = function() { randLayer(img_face); };
-		img_eyes.onload = function() { randLayer(img_eyes); };
-
-		img_silhouette.src = "/assets/img/about/silhouette.png";
-		img_shirt.src = "/assets/img/about/shirt.png";
-		img_collar.src = "/assets/img/about/collar.png";
-		img_neck.src = "/assets/img/about/neck.png";
-		img_head.src = "/assets/img/about/head.png";
-		img_face.src = "/assets/img/about/face.png";
-		img_eyes.src = "/assets/img/about/eyes.png";
+async function randLayer(layer) {
+	hidden_ctx.drawImage(layer, 0, 0);
+	img = hidden_ctx.getImageData(0, 0, hidden_canvas.width, hidden_canvas.height);
+	virtual = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	hidden_ctx.clearRect(0, 0, hidden_canvas.width, hidden_canvas.height);
+	
+	var i, r = 0, g = 0, b = 0, a = 0.8;
+	while (r == 0 && g == 0 && b == 0) { 
+		r = 255 * (Math.random() > 0.5);
+		g = 255 * (Math.random() > 0.5);
+		b = 255 * (Math.random() > 0.5);
 	}
 
-	document.addEventListener('DOMContentLoaded', init);
+	for (i = 0; i < img.data.length; i += 4) {
+		img.data[i] = r * a * img.data[i] + a * virtual.data[i];
+		img.data[i+1] = g * a * img.data[i+1] + a * virtual.data[i+1];
+		img.data[i+2] = b * a * img.data[i+2] + a * virtual.data[i+2];
+		img.data[i+3] = 255 * a;
+	}
 
-})();	
+	ctx.putImageData(img, 0, 0);		
+	return 1;
+}
+
+// randomize face but make sure to do all the layers in order
+async function randomizeFace() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	randLayer(img_silhouette);
+	randLayer(img_shirt);
+	randLayer(img_collar);
+	randLayer(img_neck);
+	randLayer(img_head);
+	randLayer(img_face);
+	randLayer(img_eyes);
+
+	return 1;
+}
+
+// initialize the canvases and preload all the images
+async function init() {
+	hidden_canvas = document.getElementById('hidden-canvas');
+	hidden_ctx = hidden_canvas.getContext('2d');
+	canvas = document.getElementById('portrait-canvas');
+	ctx = canvas.getContext('2d');
+
+	image_sources = ["/assets/img/about/silhouette.png", 
+									"/assets/img/about/shirt.png",
+									"/assets/img/about/collar.png",
+									"/assets/img/about/neck.png",
+									"/assets/img/about/head.png",
+									"/assets/img/about/face.png",
+									"/assets/img/about/eyes.png"];
+
+	Promise.all(image_sources.map(i => loadImage(i)))
+		.then((images) => {
+			img_silhouette = images[0];
+			img_shirt = images[1];
+			img_collar = images[2];
+			img_neck = images[3];
+			img_head = images[4];
+			img_face = images[5];
+			img_eyes = images[6];
+			randomizeFace();
+		});
+
+	return 1;
+}
+
+
+// set up canvas
+document.addEventListener('DOMContentLoaded', init);	
